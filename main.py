6f34b6e384b06c04116bd913d156f3a1f9720b79
Owen Wang <owenwang0099@gmail.com>
2024-11-28 13:17:48 -0500

    Added secret santa generation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import discord
 from discord.ext import commands
+from random import shuffle
 from dotenv import load_dotenv
 
 # Get token from .env
@@ -13,6 +14,7 @@ if TOKEN is None:
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 client = discord.Client(intents=intents)
 COMMAND_PREFIX = "!"
@@ -30,7 +32,7 @@ async def hello(ctx: commands.Context):
 
 
 @bot.command()
-async def ssjoin(ctx: commands.Context):
+async def join(ctx: commands.Context):
     """Join secret santa"""
 
     guild = ctx.guild
@@ -38,11 +40,11 @@ async def ssjoin(ctx: commands.Context):
         print("ERROR: Invalid guild")
         return
 
-    role = discord.utils.get(guild.roles, name="Gifter")
+    role = discord.utils.get(guild.roles, name="Secret Santa")
     if role is None:
-        print('"Gifter" role does not exist. Creating role...')
+        print('"Secret Santa" role does not exist. Creating role...')
         role = await guild.create_role(
-            name="Gifter", colour=discord.Colour.magenta(), mentionable=True
+            name="Secret Santa", colour=discord.Colour.magenta(), mentionable=True
         )
 
     author = await guild.fetch_member(ctx.author.id)
@@ -54,11 +56,11 @@ async def ssjoin(ctx: commands.Context):
     except discord.Forbidden:
         await ctx.send("I do not have permissions to manage roles!")
     else:
-        await ctx.send(f'"Gifter" role given to {author.display_name}.')
+        await ctx.send(f'"Secret Santa" role given to @{author.name}.')
 
 
 @bot.command()
-async def ssleave(ctx: commands.Context):
+async def leave(ctx: commands.Context):
     """Join secret santa"""
 
     guild = ctx.guild
@@ -66,7 +68,7 @@ async def ssleave(ctx: commands.Context):
         print("ERROR: Invalid guild")
         return
 
-    role = discord.utils.get(guild.roles, name="Gifter")
+    role = discord.utils.get(guild.roles, name="Secret Santa")
 
     author = await guild.fetch_member(ctx.author.id)
     if author is None:
@@ -77,7 +79,89 @@ async def ssleave(ctx: commands.Context):
     except discord.Forbidden:
         await ctx.send("I do not have permissions to manage roles!")
     else:
-        await ctx.send(f'"Gifter" role removed from {author.display_name}.')
+        await ctx.send(f'"Secret Santa" role removed from @{author.name}.')
+
+
+def get_santas(guild: discord.Guild):
+    """Return a list of Member with the Secret Santa role.
+
+    Args:
+        guild (discord.Guild): guild to query
+    """
+
+    role = discord.utils.get(guild.roles, name="Secret Santa")
+    if role is not None:
+        return role.members
+    return None
+
+
+@bot.command()
+async def list(ctx: commands.Context):
+    """Get all members with the Secret Santa role"""
+
+    guild = ctx.guild
+    if guild is None:
+        print("ERROR: Invalid guild")
+        return
+
+    santas = get_santas(guild)
+    if santas:
+        message = "# :santa: Secret Santa List :scroll: \n"
+        for santa in santas:
+            message += f"* {santa.display_name}\n"
+        await ctx.send(message)
+    else:
+        await ctx.send("There are no secret santas!")
+
+
+@bot.command()
+async def generate(ctx: commands.Context):
+    """Generate Secret Santa ordering and DM each user."""
+
+    guild = ctx.guild
+    if guild is None:
+        print("ERROR: Invalid guild")
+        return
+
+    if ctx.author != guild.owner:
+        await ctx.send("Generation can only be initiated by the server owner!")
+        return
+
+    santas = get_santas(guild)
+    if santas:
+        shuffle(santas)
+        can_send = []
+        cannot_send = []
+        for i in range(len(santas)):
+            dm = await bot.create_dm(santas[i])
+            try:
+                await dm.send(
+                    ":christmas_tree: Generating Secret Santas... :christmas_tree:"
+                )
+                if i == len(santas) - 1:
+                    can_send.append((dm, santas[0]))
+                else:
+                    can_send.append((dm, santas[i + 1]))
+            except discord.HTTPException:
+                cannot_send.append(santas[i])
+
+        if cannot_send:
+            for dm, _ in can_send:
+                await dm.send(
+                    "Oh no! Some users have DM's off. Generation has stopped."
+                )
+            message = ":warning: ERROR: I can't DM the following users!\n"
+            for user in cannot_send:
+                message += f"* @{user.name}\n"
+            message += "Please make sure you have enabled DM's from server members!"
+            await ctx.send(message)
+        else:
+            for dm, assigned_to in can_send:
+                await dm.send(
+                    f":christmas_tree: You are {assigned_to.display_name}'s ({assigned_to.name}) secret santa! :star:"
+                )
+    else:
+        await ctx.send("There are no secret santas!")
 
 
 # Start bot
